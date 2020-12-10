1ddce39bbfc3d2b5c80eea3bc36429532d7863ab
MWORK <phanduy.mwork@gmail.com>
2020-12-10 17:29:16 +0700

    build project

---
+++ scripts/config-sample.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "duy1",
+    //  Password
+    "Pass" : "12345",
+    //  Auth Realm
+    "Realm"   : "qa.voip.qup.vn",
+    // Display Name
+    "Display" : "Duy1",
+    // WebSocket URL
+    "WSServer"  : "ws://139.180.223.245:8080"
+};
---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+
+var user = {
+    //  User Name
+    "User" : "duy1",
+    //  Password
+    "Pass" : "12345",
+    //  Auth Realm
+    "Realm"   : "qa.voip.qup.vn",
+    // Display Name
+    "Display" : "Duy Phan",
+    // WebSocket URL
+    "WSServer"  : "wss://qa.voip.qup.vn:4443"
+};
